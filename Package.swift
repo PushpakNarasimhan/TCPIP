f898fb9ac6ef942d726108540174d940b63bbfd9
Pushpak Narasimhan <pushpak.narasimhan@gmail.com>
2020-02-06 10:56:53 +0530

    Experiment

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "TCPIP",
+    products: [
+        .library(name: "TCPIP", targets: ["TCPIP"]),
+        .library(name: "CTide", targets: ["CTide"])
+    ],
     targets: [.target(name: "TCPIP", dependencies: ["CTide"]),
               .target(name: "CTide", dependencies: [], path: "Sources/CTide")
     ]
